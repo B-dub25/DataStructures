ba549d9a29c7636e2e0953432cb38483367378ab
b-dub25 <brundel25@gmail.com>
2014-05-05 23:59:32 -0400

    fixed bug in add(int,E) and increment size when a new item is entered that I forgot to put in

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -2,11 +2,16 @@ import java.io.Serializable;
 import java.util.AbstractSequentialList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 import java.util.Queue;
 
 public class LinkedList<E> extends AbstractSequentialList<E> implements
 		List<E>, Serializable, Cloneable, Queue<E> {
 
+	/**
+	 * Computed serial ID
+	 */
+	private static final long serialVersionUID = 7532946786315141316L;
 	private Node<E> head;
 	private Node<E> last;
 	private int size;
@@ -21,38 +26,57 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements
 	@Override
 	public boolean add(E e) {
 
-		 if(head == null){
-			head = new Node<E>(e, null ,head);
-		    last = head;
-		 }
-		 else{
-			 Node<E> temp = new Node<E>(e,last, null);
-			 last.next = temp;
-			 last = temp; 
-		 }
-			return true;
-	}
-	@Override
-    public void add(int index, E e ){
-
-		if(index == 0 || head == null){
-			
+		if (head == null) {
+			head = new Node<E>(e, null, head);
+			last = head;
+		} else {
+			Node<E> temp = new Node<E>(e, last, null);
+			last.next = temp;
+			last = temp;
 		}
-    }
-	
-	public void addFirst(E e){
-		
-		if(head == null)
+		size++;
+		return true;
+	}
+
+	@Override
+	public void add(int index, E e) {
+
+		if (index == 0 || head == null) {
+			addFirst(e);
+		}
+		// Just add it to the end
+		else if (index == size - 1)
+			addLast(e);
+		else {
+			Node<E> runner = head;
+			for (int i = 0; i < index; ++i)
+				runner = runner.next;
+			Node<E> temp = new Node<E>(e, runner.previous, runner);
+			runner.previous.next = temp;
+			runner.previous = temp;
+		}
+	}
+
+	public void addFirst(E e) {
+
+		if (head == null)
 			add(e);
 		else
 			head = new Node<E>(e, null, head);
+		size++;
 	}
-	public E pop() {
-		 E temp = null;
-	     if(head != null){
-	         temp = head.type;
-	         head = head.next;
-	     }
+
+	public void addLast(E e) {
+		add(e);
+	}
+
+	public E pop() throws NoSuchElementException {
+
+		if (head == null)
+			throw new NoSuchElementException();
+		
+		final E temp = head.type;
+		head = head.next;
 		return temp;
 	}
 
@@ -62,44 +86,43 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements
 
 	@Override
 	public boolean offer(E e) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public E remove() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public E poll() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public E element() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return size;
 	}
 
 	@SuppressWarnings("hiding")
@@ -109,7 +132,7 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements
 		private Node<E> next;
 		private Node<E> previous;
 
-		private Node(E data, Node<E> past ,Node<E> nextLink) {
+		private Node(E data, Node<E> past, Node<E> nextLink) {
 
 			this.type = data;
 			this.previous = past;
@@ -118,14 +141,14 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements
 	}
 
 	public static void main(String[] args) {
+
 		LinkedList<Integer> list = new LinkedList<>();
-		list.addFirst(10);
-		list.addFirst(11);
-		list.addFirst(12);
+		list.add(0,1);
+		list.add(1,2);
+		list.add(1,3);
 		System.out.println(list.pop());
 		System.out.println(list.pop());
 		System.out.println(list.pop());
-		
 
 	}
 }
